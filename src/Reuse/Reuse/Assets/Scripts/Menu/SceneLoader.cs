0a86417a0f0ce8ce7d8bb2e2761396ca41aae439
tubplunger <143738688+tubplunger@users.noreply.github.com>
2026-05-28 13:44:01 -0700

    Menu and Demo
    Menu added into project to easily switch between all scripts. Has been built into a playable demo as well!

---
+++ src/Reuse/Reuse/Assets/Scripts/Menu/SceneLoader.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadSceneByName(string sceneName)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
